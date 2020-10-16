d0093852c357231ed769ba2d8bae5a2f75107c00
nickdn <nickdndev@gmail.com>
2020-10-16 06:13:09 +0300

    add geometry c++

--- vector_operations/src/vector_ops.h
+++ vector_operations/src/vector_ops.h
@@ -1,144 +1,161 @@
 #pragma once
 
-#include <vector>
-#include <iostream>
-#include <cmath>
 #include <cfloat>
-
+#include <cmath>
+#include <iostream>
+#include <vector>
 
 using namespace std;
 
 namespace task {
 
-    vector<double> operator+(const vector<double> &vec1, const vector<double> &vec2) {
-        vector<double> vec3;
+const double ERROR_PRECISION = 1e-6;
 
-        for (size_t i = 0; i < vec1.size(); i++)
-            vec3.push_back(vec1[i] + vec2[i]);
+vector<double> operator+(const vector<double> &vec1,
+                         const vector<double> &vec2) {
+  vector<double> vec3;
+  vec3.reserve(vec1.size());
 
-        return vec3;
-    }
+  for (size_t i = 0; i < vec1.size(); i++)
+    vec3.push_back(vec1[i] + vec2[i]);
 
-    vector<double> operator-(const vector<double> &vec1, const vector<double> &vec2) {
-        vector<double> vec3;
+  return vec3;
+}
 
-        for (size_t i = 0; i < vec1.size(); i++)
-            vec3.push_back(vec1[i] - vec2[i]);
+vector<double> operator-(const vector<double> &vec1,
+                         const vector<double> &vec2) {
+  vector<double> vec3;
+  vec3.reserve(vec1.size());
 
-        return vec3;
-    }
+  for (size_t i = 0; i < vec1.size(); i++)
+    vec3.push_back(vec1[i] - vec2[i]);
 
-    vector<double> operator+(const vector<double> &vec) {
-        vector<double> vec2;
+  return vec3;
+}
 
-        for (size_t i = 0; i < vec.size(); i++)
-            vec2.push_back(vec[i]);
+vector<double> operator+(const vector<double> &vec) {
+  vector<double> vec2;
+  vec2.reserve(vec2.size());
 
-        return vec2;
-    }
+  for (size_t i = 0; i < vec.size(); i++)
+    vec2.push_back(vec[i]);
 
-    vector<double> operator-(const vector<double> &vec) {
-        vector<double> vec2;
+  return vec2;
+}
 
-        for (size_t i = 0; i < vec.size(); i++)
-            vec2.push_back(-1 * vec[i]);
+vector<double> operator-(const vector<double> &vec) {
+  vector<double> vec2;
+  vec2.reserve(vec2.size());
 
-        return vec2;
-    }
+  for (size_t i = 0; i < vec.size(); i++)
+    vec2.push_back(-1 * vec[i]);
 
-    double operator*(const vector<double> &vector1, const vector<double> &vector2) {
-        double dot_product = 0;
+  return vec2;
+}
 
-        for (size_t i = 0; i < vector1.size(); i++)
-            dot_product += vector1[i] * vector2[i];
+double operator*(const vector<double> &vector1, const vector<double> &vector2) {
+  double dot_product = 0;
 
-        return dot_product;
-    }
+  for (size_t i = 0; i < vector1.size(); i++)
+    dot_product += vector1[i] * vector2[i];
 
-    vector<double> operator%(const vector<double> &vec1, const vector<double> &vec2) {
-        vector<double> vec3(3, 0);
+  return dot_product;
+}
 
-        vec3[0] = vec1[1] * vec2[2] - vec1[2] * vec2[1];
-        vec3[1] = -1 * (vec1[0] * vec2[2] - vec1[2] * vec2[0]);
-        vec3[2] = vec1[0] * vec2[1] - vec1[1] * vec2[0];
+vector<double> operator%(const vector<double> &vec1,
+                         const vector<double> &vec2) {
+  vector<double> vec3(3, 0);
 
-        return vec3;
-    }
+  vec3[0] = vec1[1] * vec2[2] - vec1[2] * vec2[1];
+  vec3[1] = -1 * (vec1[0] * vec2[2] - vec1[2] * vec2[0]);
+  vec3[2] = vec1[0] * vec2[1] - vec1[1] * vec2[0];
 
-    bool operator||(const vector<double> &vec1, const vector<double> &vec2) {
+  return vec3;
+}
 
-        double v1 = 0;
-        double v2 = 0;
+double normVector(const vector<double> &vec1) {
+  double norm = 0;
 
-        for (size_t i = 0; i < vec1.size(); i++) {
-            v1 += vec1[i] * vec1[i];
-            v2 += vec2[i] * vec2[i];
-        }
+  for (double item : vec1) {
+    norm += item * item;
+  }
 
-        double res = abs((vec1 * vec2) / (sqrt(v1) * sqrt(v2)));
-        return abs(res - 1) < 0.00000000000001;
-    }
+  return norm;
+}
 
-    bool operator&&(const vector<double> &vec1, const vector<double> &vec2) {
+bool operator||(const vector<double> &vec1, const vector<double> &vec2) {
 
-        double v1 = 0;
-        double v2 = 0;
+  double normVec1 = normVector(vec1);
+  double normVec2 = normVector(vec2);
 
-        for (size_t i = 0; i < vec1.size(); i++) {
-            v1 += vec1[i] * vec1[i];
-            v2 += vec2[i] * vec2[i];
-        }
-        double scal_result = vec1 * vec2;
-        double res = (scal_result) / (sqrt(v1) * sqrt(v2));
-        return abs(res - 1) < 0.00000000000001 && scal_result > 0;
-    }
+  if (normVec1 <= 0 || normVec2 <= 0)
+    return true;
 
-    void reverse(vector<double> &vec1) {
-        for (size_t i = 0; i < vec1.size() / 2; i++)
-            swap(vec1[i], vec1[vec1.size() - 1 - i]);
-    }
+  double res = abs((vec1 * vec2) / (sqrt(normVec1) * sqrt(normVec2)));
+  return abs(res - 1) < ERROR_PRECISION;
+}
 
-    vector<int> operator|(const vector<int> &vec1, const vector<int> &vec2) {
-        vector<int> vec3;
+bool operator&&(const vector<double> &vec1, const vector<double> &vec2) {
 
-        for (size_t i = 0; i < vec1.size(); i++)
-            vec3.push_back(vec1[i] | vec2[i]);
+  double normVec1 = normVector(vec1);
+  double normVec2 = normVector(vec2);
 
-        return vec3;
-    }
+  if (normVec1 <= 0 || normVec2 <= 0)
+    return true;
 
-    vector<int> operator&(const vector<int> &vec1, const vector<int> &vec2) {
-        vector<int> vec3;
+  double scal_result = vec1 * vec2;
+  double res = (scal_result) / (sqrt(normVec1) * sqrt(normVec2));
+  return abs(res - 1) < ERROR_PRECISION && scal_result > 0;
+}
 
-        for (size_t i = 0; i < vec1.size(); i++)
-            vec3.push_back(vec1[i] & vec2[i]);
+void reverse(vector<double> &vec1) {
+  for (size_t i = 0; i < vec1.size() / 2; i++)
+    swap(vec1[i], vec1[vec1.size() - 1 - i]);
+}
 
-        return vec3;
-    }
+vector<int> operator|(const vector<int> &vec1, const vector<int> &vec2) {
+  vector<int> vec3;
+  vec3.reserve(vec1.size());
 
-    stringstream &operator>>(stringstream &stream, vector<double> &vec) {
-        int size;
-        double item;
+  for (size_t i = 0; i < vec1.size(); i++)
+    vec3.push_back(vec1[i] | vec2[i]);
 
-        stream >> size;
+  return vec3;
+}
 
-        vec.resize(size);
+vector<int> operator&(const vector<int> &vec1, const vector<int> &vec2) {
+  vector<int> vec3;
+  vec3.reserve(vec1.size());
 
-        for (size_t i = 0; i < size; i++) {
-            stream >> item;
-            vec[i] = item;
-        }
+  for (size_t i = 0; i < vec1.size(); i++)
+    vec3.push_back(vec1[i] & vec2[i]);
 
-        return stream;
-    }
+  return vec3;
+}
 
-    stringstream &operator<<(stringstream &stream, const vector<double> &vec) {
-        for (double i : vec) {
-            stream << i << ' ';
-        }
+stringstream &operator>>(stringstream &stream, vector<double> &vec) {
+  int size;
+  double item;
 
-        stream << '\n';
+  stream >> size;
 
-        return stream;
-    }
+  vec.resize(size);
+
+  for (size_t i = 0; i < size; i++) {
+    stream >> item;
+    vec[i] = item;
+  }
+
+  return stream;
+}
+
+stringstream &operator<<(stringstream &stream, const vector<double> &vec) {
+  for (double i : vec) {
+    stream << i << ' ';
+  }
+
+  stream << '\n';
+
+  return stream;
+}
 }
