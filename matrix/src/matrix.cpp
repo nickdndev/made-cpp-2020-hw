b12890a31ce5db083bffd0ba3306b215667acbf5
nickdn <nickdndev@gmail.com>
2020-10-16 07:05:39 +0300

    ONLY CHANGE FORMAT TO CLANG_FORMAT

--- matrix/src/matrix.cpp
+++ matrix/src/matrix.cpp
@@ -1,411 +1,408 @@
 #include "matrix.h"
 #include <cmath>
 #include <cstring>
-#include <string>
 #include <iostream>
 #include <sstream>
+#include <string>
 
 using namespace task;
 
-Matrix::Matrix() : columns(1), rows(1) {
-    initData();
-}
+Matrix::Matrix() : Matrix(1, 1) {}
 
 Matrix::Matrix(size_t rows, size_t cols) : rows(rows), columns(cols) {
-    initData();
+
+  data = new double *[rows];
+
+  for (size_t i = 0; i < rows; i++) {
+    data[i] = new double[columns];
+    for (size_t j = 0; j < columns; j++) {
+      if (i == j)
+        data[i][j] = 1.;
+      else
+        data[i][j] = 0;
+    }
+  }
 }
 
 Matrix::Matrix(const Matrix &copy) {
-    columns = copy.columns;
-    rows = copy.rows;
+  columns = copy.columns;
+  rows = copy.rows;
 
-    data = new double *[rows];
-    for (size_t i = 0; i < rows; i++) {
-        data[i] = new double[columns];
-        for (size_t j = 0; j < columns; j++) {
-            data[i][j] = copy.data[i][j];
-        }
+  data = new double *[rows];
+  for (size_t i = 0; i < rows; i++) {
+    data[i] = new double[columns];
+    for (size_t j = 0; j < columns; j++) {
+      data[i][j] = copy.data[i][j];
     }
+  }
 }
 
+double *Matrix::operator[](size_t row) { return data[row]; }
 
-double *Matrix::operator[](size_t row) {
-    return data[row];
-}
-
-double *Matrix::operator[](size_t row) const {
-    return data[row];
-}
+double *Matrix::operator[](size_t row) const { return data[row]; }
 
 Matrix &Matrix::operator=(const Matrix &a) {
-    if (this == &a)
-        return *this;
-
-    auto **new_data = new double *[a.rows];
-
-    for (size_t i = 0; i < a.rows; i++) {
-        new_data[i] = new double[a.columns];
-        for (size_t j = 0; j < a.columns; j++) {
-            new_data[i][j] = a.data[i][j];
-        }
-    }
-
-
-    for (size_t i = 0; i < rows; i++) {
-        delete[] data[i];
-    }
-
-    delete data;
-
-    data = new_data;
-
-    columns = a.columns;
-    rows = a.rows;
-
+  if (this == &a)
     return *this;
+
+  auto **new_data = new double *[a.rows];
+
+  for (size_t i = 0; i < a.rows; i++) {
+    new_data[i] = new double[a.columns];
+    for (size_t j = 0; j < a.columns; j++) {
+      new_data[i][j] = a.data[i][j];
+    }
+  }
+
+  for (size_t i = 0; i < rows; i++) {
+    delete[] data[i];
+  }
+
+  delete data;
+
+  data = new_data;
+
+  columns = a.columns;
+  rows = a.rows;
+
+  return *this;
 }
 
 double &Matrix::get(size_t row, size_t col) {
-    if (rows < row + 1 || columns < col + 1) {
-        throw OutOfBoundsException();
-    }
+  if (rows < row + 1 || columns < col + 1) {
+    throw OutOfBoundsException();
+  }
 
-    return data[row][col];
+  return data[row][col];
 }
 
 const double &Matrix::get(size_t row, size_t col) const {
-    if (rows < row + 1 || columns < col + 1) {
-        throw OutOfBoundsException();
-    }
-    return data[row][col];
+  if (rows < row + 1 || columns < col + 1) {
+    throw OutOfBoundsException();
+  }
+  return data[row][col];
 }
 
 void Matrix::set(size_t row, size_t col, const double &value) {
-    if (rows < row + 1 || columns < col + 1) {
-        throw OutOfBoundsException();
-    }
-    data[row][col] = value;
+  if (rows < row + 1 || columns < col + 1) {
+    throw OutOfBoundsException();
+  }
+  data[row][col] = value;
 }
 
 void Matrix::resize(size_t new_rows, size_t new_cols) {
 
-    double **new_data = new double *[new_cols];
+  double **new_data = new double *[new_cols];
 
-    for (size_t i = 0; i < new_rows; i++) {
-        new_data[i] = new double[new_cols];
-        for (size_t j = 0; j < new_cols; j++) {
-            if (j > columns - 1) {
-                new_data[i][j] = 0;
-            } else {
-                if (i > rows - 1) {
-                    new_data[i][j] = 0;
-                } else {
-                    new_data[i][j] = data[i][j];
-                }
-
-            }
+  for (size_t i = 0; i < new_rows; i++) {
+    new_data[i] = new double[new_cols];
+    for (size_t j = 0; j < new_cols; j++) {
+      if (j > columns - 1) {
+        new_data[i][j] = 0;
+      } else {
+        if (i > rows - 1) {
+          new_data[i][j] = 0;
+        } else {
+          new_data[i][j] = data[i][j];
         }
+      }
     }
+  }
 
-    for (size_t i = 0; i < rows; i++) {
-        delete[] data[i];
-    }
+  for (size_t i = 0; i < rows; i++) {
+    delete[] data[i];
+  }
 
-    delete[] data;
+  delete[] data;
 
-    columns = new_cols;
-    rows = new_rows;
-    data = new_data;
-
+  columns = new_cols;
+  rows = new_rows;
+  data = new_data;
 }
 
 std::vector<double> Matrix::getRow(size_t row) {
 
-    std::vector<double> vec(columns);
-    for (size_t i = 0; i < columns; i++) {
-        vec[i] = data[row][i];
-    }
-    return vec;
+  std::vector<double> vec(columns);
+  for (size_t i = 0; i < columns; i++) {
+    vec[i] = data[row][i];
+  }
+  return vec;
 }
 
 std::vector<double> Matrix::getColumn(size_t column) {
-    std::vector<double> vec(rows);
-    for (size_t i = 0; i < rows; i++) {
-        vec[i] = data[i][column];
-    }
-    return vec;
+  std::vector<double> vec(rows);
+  for (size_t i = 0; i < rows; i++) {
+    vec[i] = data[i][column];
+  }
+  return vec;
 }
 
 Matrix Matrix::operator*(const Matrix &a) const {
-    Matrix m = *this;
-    return m *= a;
+  Matrix m = *this;
+  return m *= a;
 }
 
 Matrix Matrix::operator*(const double &a) const {
-    Matrix m = *this;
-    return m *= a;
+  Matrix m = *this;
+  return m *= a;
 }
 
 Matrix task::operator*(const double &a, const Matrix &b) {
-    Matrix m = b;
-    return m *= a;
+  Matrix m = b;
+  return m *= a;
 };
 
 Matrix &Matrix::operator*=(const Matrix &a) {
-    if (columns != a.rows)
-        throw SizeMismatchException();
+  if (columns != a.rows)
+    throw SizeMismatchException();
 
+  auto **new_data = new double *[rows];
 
-    auto **new_data = new double *[rows];
-
-    for (size_t i = 0; i < rows; i++) {
-        new_data[i] = new double[a.columns];
-        for (size_t j = 0; j < a.columns; j++) {
-            new_data[i][j] = 0;
-            for (size_t k = 0; k < columns; k++)
-                new_data[i][j] += data[i][k] * a[k][j];
-        }
+  for (size_t i = 0; i < rows; i++) {
+    new_data[i] = new double[a.columns];
+    for (size_t j = 0; j < a.columns; j++) {
+      new_data[i][j] = 0;
+      for (size_t k = 0; k < columns; k++)
+        new_data[i][j] += data[i][k] * a[k][j];
     }
+  }
 
-    for (size_t i = 0; i < rows; i++) {
-        delete[] data[i];
-    }
+  for (size_t i = 0; i < rows; i++) {
+    delete[] data[i];
+  }
 
-    delete[] data;
+  delete[] data;
 
-    columns = a.columns;
-    data = new_data;
+  columns = a.columns;
+  data = new_data;
 
-    return *this;
+  return *this;
 }
 
 Matrix &Matrix::operator*=(const double &number) {
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < columns; j++) {
-            data[i][j] = data[i][j] * number;
-        }
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < columns; j++) {
+      data[i][j] = data[i][j] * number;
     }
+  }
 
-    return *this;
+  return *this;
 }
 
 Matrix &Matrix::operator+=(const Matrix &a) {
-    if (columns != a.columns || rows != a.rows)
-        throw SizeMismatchException();
+  if (columns != a.columns || rows != a.rows)
+    throw SizeMismatchException();
 
-    for (size_t i = 0; i < a.rows; i++) {
-        for (size_t j = 0; j < a.columns; j++) {
-            data[i][j] += a.data[i][j];
-        }
+  for (size_t i = 0; i < a.rows; i++) {
+    for (size_t j = 0; j < a.columns; j++) {
+      data[i][j] += a.data[i][j];
     }
+  }
 
-    return *this;
+  return *this;
 }
 
 Matrix &Matrix::operator-=(const Matrix &a) {
-    if (columns != a.columns || rows != a.rows)
-        throw SizeMismatchException();
+  if (columns != a.columns || rows != a.rows)
+    throw SizeMismatchException();
 
-    for (size_t i = 0; i < a.rows; i++) {
-        for (size_t j = 0; j < a.columns; j++) {
-            data[i][j] -= a.data[i][j];
-        }
+  for (size_t i = 0; i < a.rows; i++) {
+    for (size_t j = 0; j < a.columns; j++) {
+      data[i][j] -= a.data[i][j];
     }
+  }
 
-    return *this;
+  return *this;
 }
 
 Matrix Matrix::operator+(const Matrix &a) const {
-    Matrix m = *this;
-    return m += a;
+  Matrix m = *this;
+  return m += a;
 }
 
 Matrix Matrix::operator-(const Matrix &a) const {
-    Matrix m = *this;
-    return m -= a;
+  Matrix m = *this;
+  return m -= a;
 }
 
 double Matrix::trace() const {
-    if (columns != rows)
-        throw SizeMismatchException();
+  if (columns != rows)
+    throw SizeMismatchException();
 
-    double sum = 0;
-    for (int i = 0; i < rows; i++)
-        sum += data[i][i];
-    return sum;
+  double sum = 0;
+  for (int i = 0; i < rows; i++)
+    sum += data[i][i];
+  return sum;
 }
 
 bool Matrix::operator==(const Matrix &a) const {
-    if (columns != a.columns || rows != a.rows)
+  if (columns != a.columns || rows != a.rows)
+    return false;
+
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < columns; j++) {
+      if (std::abs(data[i][j] - a.data[i][j]) > EPS)
         return false;
-
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < columns; j++) {
-            if (std::abs(data[i][j] - a.data[i][j]) > EPS)
-                return false;
-        }
     }
+  }
 
-    return true;
+  return true;
 }
 
-bool Matrix::operator!=(const Matrix &a) const {
-    return !(*this == a);
-}
+bool Matrix::operator!=(const Matrix &a) const { return !(*this == a); }
 
 std::ostream &task::operator<<(std::ostream &output, const Matrix &matrix) {
-    for (size_t i = 0; i < matrix.rows; i++) {
-        for (size_t j = 0; j < matrix.columns; j++) {
-            output << matrix.data[i][j] << ' ';
-        }
-
-        output << std::endl;
+  for (size_t i = 0; i < matrix.rows; i++) {
+    for (size_t j = 0; j < matrix.columns; j++) {
+      output << matrix.data[i][j] << ' ';
     }
 
     output << std::endl;
+  }
 
-    return output;
+  output << std::endl;
+
+  return output;
 }
 
-
 std::istream &task::operator>>(std::istream &input, Matrix &matrix) {
-    size_t rows, columns;
+  size_t rows, columns;
 
-    input >> rows >> columns;
+  input >> rows >> columns;
 
-    for (size_t i = 0; i < matrix.rows; i++) {
-        delete[] matrix.data[i];
+  for (size_t i = 0; i < matrix.rows; i++) {
+    delete[] matrix.data[i];
+  }
+
+  delete[] matrix.data;
+
+  matrix.data = new double *[rows];
+
+  double value;
+
+  for (size_t i = 0; i < rows; i++) {
+    matrix.data[i] = new double[columns];
+    for (size_t j = 0; j < columns; j++) {
+      input >> value;
+      matrix.data[i][j] = value;
     }
+  }
 
-    delete[] matrix.data;
+  matrix.rows = rows;
+  matrix.columns = columns;
 
-    matrix.data = new double *[rows];
-
-    double value;
-
-    for (size_t i = 0; i < rows; i++) {
-        matrix.data[i] = new double[columns];
-        for (size_t j = 0; j < columns; j++) {
-            input >> value;
-            matrix.data[i][j] = value;
-        }
-    }
-
-    matrix.rows = rows;
-    matrix.columns = columns;
-
-    return input;
+  return input;
 }
 
 Matrix Matrix::operator-() const {
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < columns; j++) {
-            data[i][j] *= -1;
-        }
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < columns; j++) {
+      data[i][j] *= -1;
     }
+  }
 
-    return *this;
+  return *this;
 }
 
 Matrix Matrix::operator+() const {
-    for (size_t i = 0; i < rows; i++) {
-        for (size_t j = 0; j < columns; j++) {
-            data[i][j] *= -1;
-        }
+  for (size_t i = 0; i < rows; i++) {
+    for (size_t j = 0; j < columns; j++) {
+      data[i][j] *= -1;
     }
+  }
 
-    return *this;
+  return *this;
 }
 
 void Matrix::transpose() {
 
-    auto **new_data = new double *[columns];
+  auto **new_data = new double *[columns];
 
-    for (int i = 0; i < columns; i++) {
-        new_data[i] = new double[rows];
-        for (int j = 0; j < rows; j++) {
-            new_data[i][j] = data[j][i];
-        }
+  for (int i = 0; i < columns; i++) {
+    new_data[i] = new double[rows];
+    for (int j = 0; j < rows; j++) {
+      new_data[i][j] = data[j][i];
     }
+  }
 
-    for (size_t i = 0; i < rows; i++) {
-        delete[] data[i];
-    }
+  for (size_t i = 0; i < rows; i++) {
+    delete[] data[i];
+  }
 
-    delete[] data;
+  delete[] data;
 
-    size_t tmp = rows;
-    rows = columns;
-    columns = tmp;
-    data = new_data;
+  size_t tmp = rows;
+  rows = columns;
+  columns = tmp;
+  data = new_data;
 }
 
 Matrix Matrix::transposed() const {
 
-    Matrix transposed_matrix(columns, rows);
+  Matrix transposed_matrix(columns, rows);
 
-    for (size_t i = 0; i < columns; i++) {
-        transposed_matrix.data[i] = new double[rows];
-        for (size_t j = 0; j < rows; j++) {
-            transposed_matrix.data[i][j] = data[j][i];
-        }
+  for (size_t i = 0; i < columns; i++) {
+    transposed_matrix.data[i] = new double[rows];
+    for (size_t j = 0; j < rows; j++) {
+      transposed_matrix.data[i][j] = data[j][i];
     }
+  }
 
-    return transposed_matrix;
+  return transposed_matrix;
 }
 
 void cofactor(double **mat, double **temp, size_t p, size_t q, size_t n) {
-    size_t i = 0;
-    size_t j = 0;
-    for (size_t row = 0; row < n; row++) {
-        for (size_t col = 0; col < n; col++) {
-            if (row != p && col != q) {
-                temp[i][j++] = mat[row][col];
-                if (j == n - 1) {
-                    j = 0;
-                    i++;
-                }
-            }
+  size_t i = 0;
+  size_t j = 0;
+  for (size_t row = 0; row < n; row++) {
+    for (size_t col = 0; col < n; col++) {
+      if (row != p && col != q) {
+        temp[i][j++] = mat[row][col];
+        if (j == n - 1) {
+          j = 0;
+          i++;
         }
+      }
     }
+  }
 }
 
 double calculateDeterminantfMatrix(double **mat, size_t n) {
-    double determinant = 0.;
+  double determinant = 0.;
 
-    if (n == 1)
-        return mat[0][0];
+  if (n == 1)
+    return mat[0][0];
 
-    auto **temp = new double *[n];
+  auto **temp = new double *[n];
 
-    for (size_t i = 0; i < n; i++) {
-        temp[i] = new double[n];
-        for (size_t j = 0; j < n; j++) {
-            for (size_t k = 0; k < n; k++)
-                temp[i][j] = mat[i][j];
-        }
+  for (size_t i = 0; i < n; i++) {
+    temp[i] = new double[n];
+    for (size_t j = 0; j < n; j++) {
+      for (size_t k = 0; k < n; k++)
+        temp[i][j] = mat[i][j];
     }
+  }
 
-    int sign = 1;
+  int sign = 1;
 
-    for (size_t f = 0; f < n; f++) {
-        cofactor(mat, temp, 0, f, n);
-        determinant += sign * mat[0][f] * calculateDeterminantfMatrix(temp, n - 1);
-        sign = -sign;
-    }
+  for (size_t f = 0; f < n; f++) {
+    cofactor(mat, temp, 0, f, n);
+    determinant += sign * mat[0][f] * calculateDeterminantfMatrix(temp, n - 1);
+    sign = -sign;
+  }
 
+  for (size_t i = 0; i < n; i++) {
+    delete[] temp[i];
+  }
 
-    for (size_t i = 0; i < n; i++) {
-        delete[] temp[i];
-    }
+  delete[] temp;
 
-    delete[] temp;
-
-    return determinant;
+  return determinant;
 }
 
 double Matrix::det() const {
-    if (columns != rows)
-        throw SizeMismatchException();
-    return calculateDeterminantfMatrix(data, rows);
+  if (columns != rows)
+    throw SizeMismatchException();
+
+  return calculateDeterminantfMatrix(data, rows);
 }
--- matrix/src/matrix.h
+++ matrix/src/matrix.h
@@ -1,93 +1,72 @@
 #pragma once
 
-#include <vector>
 #include <iostream>
-
+#include <vector>
 
 namespace task {
 
 const double EPS = 1e-6;
 
-
 class OutOfBoundsException : public std::exception {};
 class SizeMismatchException : public std::exception {};
 
-
 class Matrix {
 
-    size_t columns;
-    size_t rows;
+  size_t columns;
+  size_t rows;
 
-    double **data;
+  double **data;
 
-    friend std::ostream &operator<<(std::ostream &, const Matrix &);
+  friend std::ostream &operator<<(std::ostream &, const Matrix &);
 
-    friend std::istream &operator>>(std::istream &, Matrix &);
+  friend std::istream &operator>>(std::istream &, Matrix &);
 
-    friend Matrix operator*(const double &a, const Matrix &b);
+  friend Matrix operator*(const double &a, const Matrix &b);
 
-    void initData() {
-
-        data = new double *[rows];
-
-        for (size_t i = 0; i < rows; i++) {
-            data[i] = new double[columns];
-            for (size_t j = 0; j < columns; j++) {
-                if (i == j)
-                    data[i][j] = 1.;
-                else
-                    data[i][j] = 0;
-            }
-        }
-    }
+  friend Matrix operator*(const double &a, const Matrix &b);
 
 public:
+  Matrix();
+  Matrix(size_t rows, size_t cols);
+  Matrix(const Matrix &copy);
+  Matrix &operator=(const Matrix &a);
 
-    Matrix();
-    Matrix(size_t rows, size_t cols);
-    Matrix(const Matrix& copy);
-    Matrix& operator=(const Matrix& a);
+  double &get(size_t row, size_t col);
+  const double &get(size_t row, size_t col) const;
+  void set(size_t row, size_t col, const double &value);
+  void resize(size_t new_rows, size_t new_cols);
 
-    double& get(size_t row, size_t col);
-    const double& get(size_t row, size_t col) const;
-    void set(size_t row, size_t col, const double& value);
-    void resize(size_t new_rows, size_t new_cols);
+  double *operator[](size_t row);
+  double *operator[](size_t row) const;
 
-    double *operator[](size_t row);
-    double *operator[](size_t row) const;
+  Matrix &operator+=(const Matrix &a);
+  Matrix &operator-=(const Matrix &a);
+  Matrix &operator*=(const Matrix &a);
+  Matrix &operator*=(const double &number);
 
-    Matrix& operator+=(const Matrix& a);
-    Matrix& operator-=(const Matrix& a);
-    Matrix& operator*=(const Matrix& a);
-    Matrix& operator*=(const double& number);
+  Matrix operator+(const Matrix &a) const;
+  Matrix operator-(const Matrix &a) const;
+  Matrix operator*(const Matrix &a) const;
+  Matrix operator*(const double &a) const;
 
-    Matrix operator+(const Matrix& a) const;
-    Matrix operator-(const Matrix& a) const;
-    Matrix operator*(const Matrix& a) const;
-    Matrix operator*(const double& a) const;
+  Matrix operator-() const;
+  Matrix operator+() const;
 
-    Matrix operator-() const;
-    Matrix operator+() const;
+  double det() const;
+  void transpose();
+  Matrix transposed() const;
+  double trace() const;
 
-    double det() const;
-    void transpose();
-    Matrix transposed() const;
-    double trace() const;
+  std::vector<double> getRow(size_t row);
+  std::vector<double> getColumn(size_t column);
 
-    std::vector<double> getRow(size_t row);
-    std::vector<double> getColumn(size_t column);
-
-    bool operator==(const Matrix& a) const;
-    bool operator!=(const Matrix& a) const;
-
+  bool operator==(const Matrix &a) const;
+  bool operator!=(const Matrix &a) const;
 };
 
+Matrix operator*(const double &a, const Matrix &b);
 
-Matrix operator*(const double& a, const Matrix& b);
+std::ostream &operator<<(std::ostream &output, const Matrix &matrix);
+std::istream &operator>>(std::istream &input, Matrix &matrix);
 
-std::ostream& operator<<(std::ostream& output, const Matrix& matrix);
-std::istream& operator>>(std::istream& input, Matrix& matrix);
-
-
-
-}
+} // namespace task
