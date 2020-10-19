51925a2c235e37dca772fd40c368a64f04d0831d
nickdn <nickdndev@gmail.com>
2020-10-20 02:10:51 +0300

    clean code

--- chuck_allocator/main.cpp
+++ chuck_allocator/main.cpp
@@ -6,8 +6,6 @@
 #include <list>
 #include <map>
 #include <set>
-#include <unordered_map>
-#include <unordered_set>
 #include <utility>
 #include <vector>
 
@@ -22,54 +20,50 @@ template <std::size_t CHUNK_SIZE> class Chunk {
 public:
   Chunk() {
     blocks.resize(CHUNK_SIZE);
+
     auto *pointer_header = reinterpret_cast<std::uint32_t *>(blocks.data());
+
     *pointer_header = CHUNK_SIZE - HEADER_LENGTH;
     point_end_block = pointer_header;
     free_blocks.insert(pointer_header);
   }
 
   bool contains(const std::uint8_t *address) const noexcept {
-    const auto *start_chunk_address =
+    const auto *start_chunk =
         reinterpret_cast<const std::uint8_t *>(blocks.data());
-    const std::uint8_t *end_chunk_address = start_chunk_address + CHUNK_SIZE;
-    return (start_chunk_address <= address) && (address <= end_chunk_address);
+    const auto *end_chunk = start_chunk + CHUNK_SIZE;
+    return (start_chunk <= address) && (address <= end_chunk);
   }
 
   std::uint8_t *reserveBlock(std::size_t allocation_size) {
-    const std::size_t not_aligned_address =
-        reinterpret_cast<std::size_t>(point_end_block) + allocation_size;
-    /*  const std::size_t alignment_padding =
-          getAlignmentPadding(not_aligned_address, HEADER_LENGTH);*/
-    const auto alloc_size_with_alignment =
-        static_cast<std::uint32_t>(allocation_size /* + alignment_padding*/);
+    const auto alloc_size_req = static_cast<std::uint32_t>(allocation_size);
 
-    if ((!point_end_block) || (alloc_size_with_alignment > *point_end_block)) {
+    if ((!point_end_block) || (alloc_size_req > *point_end_block)) {
       return nullptr;
     }
 
-    const auto min_it = std::min_element(
-        free_blocks.cbegin(), free_blocks.cend(),
-        [alloc_size_with_alignment](const std::uint32_t *lhs,
-                                    const std::uint32_t *rhs) {
-          if (*rhs < alloc_size_with_alignment) {
-            return true;
-          }
+    auto comp_min_block = [alloc_size_req](const std::uint32_t *lhs,
+                                           const std::uint32_t *rhs) {
+      if (*rhs < alloc_size_req)
+        return true;
 
-          return (*lhs < *rhs) && (*lhs >= alloc_size_with_alignment);
-        });
+      return (*lhs < *rhs) && (*lhs >= alloc_size_req);
+    };
 
-    std::uint32_t *header_address = *min_it;
+    const auto min_item = std::min_element(free_blocks.cbegin(),
+                                           free_blocks.cend(), comp_min_block);
+
+    std::uint32_t *header_address = *min_item;
     auto *new_header_address = reinterpret_cast<std::uint32_t *>(
         reinterpret_cast<std::uint8_t *>(header_address) + HEADER_LENGTH +
-            alloc_size_with_alignment);
+            alloc_size_req);
 
     if (free_blocks.find(new_header_address) == free_blocks.cend()) {
       const std::uint32_t old_block_size = *header_address;
       const std::uint32_t difference = old_block_size - HEADER_LENGTH;
 
-      if (difference >= alloc_size_with_alignment) {
-        const std::uint32_t new_block_size =
-            difference - alloc_size_with_alignment;
+      if (difference >= alloc_size_req) {
+        const std::uint32_t new_block_size = difference - alloc_size_req;
         *new_header_address = new_block_size;
         free_blocks.insert(new_header_address);
       }
@@ -81,11 +75,13 @@ public:
 
     if (header_address == point_end_block) {
 
-      const auto max_it = std::max_element(
-          free_blocks.cbegin(), free_blocks.cend(),
-          [](const std::uint32_t *lhs, const std::uint32_t *rhs) {
-            return (*lhs) < (*rhs);
-          });
+      auto comp_max_block = [](const std::uint32_t *lhs,
+                               const std::uint32_t *rhs) {
+        return (*lhs) < (*rhs);
+      };
+
+      const auto max_it = std::max_element(free_blocks.cbegin(),
+                                           free_blocks.cend(), comp_max_block);
 
       point_end_block = (max_it != free_blocks.cend()) ? (*max_it) : (nullptr);
     }
@@ -102,19 +98,13 @@ public:
     }
 
     free_blocks.insert(reinterpret_cast<std::uint32_t *>(header_address));
-    /* auto forward_it =
-         free_blocks.find(reinterpret_cast<std::uint32_t *>(header_address));*/
   }
 };
 
 template <std::size_t CHUNK_SIZE> class AllocationMemory {
-  std::deque<Chunk<CHUNK_SIZE>> chunks{1};
+  std::deque<Chunk<CHUNK_SIZE>> chunks;
 
 public:
-  AllocationMemory() = default;
-
-  ~AllocationMemory() = default;
-  ;
 
   void *allocate_object(std::size_t size) {
 
