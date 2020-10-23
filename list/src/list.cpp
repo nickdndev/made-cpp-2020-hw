170f6e196062fe2b75e6d976951f983f1a05ba1b
nickdn <nickdndev@gmail.com>
2020-10-23 12:07:14 +0300

    inti commit for list

--- list/src/list.cpp
+++ list/src/list.cpp
@@ -1,3 +1,126 @@
 #include "list.h"
 
-// Your code goes here...
+namespace task {
+
+template <class T, class Alloc> list<T, Alloc>::list() {
+  count = 0;
+
+  Node *nodes = allocator.allocate(2);
+  allocator.construct(nodes);
+  allocator.construct(nodes + 1);
+  first = nodes;
+  last = nodes + 1;
+  first->next = nodes;
+  last->prev = first;
+}
+template <class T, class Alloc> list<T, Alloc>::~list() {}
+
+template <class T, class Alloc> void list<T, Alloc>::push_back(T &&value) {
+  insert(cend(), std::move(value));
+}
+
+template <class T, class Alloc>
+typename list<T, Alloc>::iterator list<T, Alloc>::insert(const_iterator pos,
+                                                         const T &value) {
+
+  auto *p = pos.current;
+  count++;
+
+  Node *node = allocator.allocate(1);
+  allocator.construct(node, p->prev, p);
+
+  p->prev->next = node;
+  p->prev = node;
+  return iterator(node);
+}
+
+template <class T, class Alloc>
+typename list<T, Alloc>::iterator
+list<T, Alloc>::insert(list::const_iterator pos, T &&value) {
+
+  auto *p = pos.current;
+  count++;
+
+  Node *node = allocator.allocate(1);
+  allocator.construct(node, value, p->prev, p);
+
+  p->prev->next = node;
+  p->prev = node;
+  return iterator(node);
+}
+
+template <class T, class Alloc>
+typename list<T, Alloc>::iterator list<T, Alloc>::begin() {
+  if (!empty()) {
+    iterator itr{first->next};
+    return itr;
+  }
+}
+
+template <class T, class Alloc>
+typename list<T, Alloc>::iterator list<T, Alloc>::end() {
+  return list::iterator();
+}
+template <class T, class Alloc>
+typename list<T, Alloc>::const_iterator list<T, Alloc>::cend() const {
+  const_iterator itr{last};
+  return itr;
+}
+template <class T, class Alloc>
+typename list<T, Alloc>::const_iterator list<T, Alloc>::cbegin() const {
+  if (!empty()) {
+    const_iterator const_itr{first->next};
+    return const_itr;
+  }
+}
+
+template <class T, class Alloc> size_t list<T, Alloc>::size() const {
+  return count;
+}
+template <class T, class Alloc> void list<T, Alloc>::pop_front() {
+
+  auto bei = cbegin();
+  erase(cbegin());
+}
+
+template <class T, class Alloc>
+typename list<T, Alloc>::iterator
+list<T, Alloc>::erase(list::const_iterator pos) {
+  auto *p = pos.current;
+  // typename iterator value{p->next};
+  p->prev->next = p->next;
+  p->next->prev = p->prev;
+  // delete p;
+
+  allocator.deallocate(p, 1);
+  count--;
+  return iterator(p->next);
+}
+template <class T, class Alloc> bool list<T, Alloc>::empty() const {
+  return size() == 0;
+}
+template <class T, class Alloc> T &list<T, Alloc>::front() { return *begin(); }
+
+template <class T, class Alloc> list<T, Alloc>::iterator::iterator() {}
+
+template <class T, class Alloc>
+list<T, Alloc>::iterator::iterator(const list<T, Alloc>::iterator &) {}
+template <class T, class Alloc>
+list<T, Alloc>::iterator::iterator(list::Node *p) : current{p} {}
+
+template <class T, class Alloc>
+typename list<T, Alloc>::iterator::reference
+list<T, Alloc>::iterator::operator*() const {
+  return current->data;
+}
+
+template <class T, class Alloc>
+list<T, Alloc>::const_iterator::const_iterator() : current{nullptr} {}
+
+template <class T, class Alloc>
+list<T, Alloc>::const_iterator::const_iterator(const list::const_iterator &) =
+default;
+
+template <class T, class Alloc>
+list<T, Alloc>::const_iterator::const_iterator(list::Node *p) : current{p} {}
+} // namespace task
--- list/src/list.h
+++ list/src/list.h
@@ -1,134 +1,176 @@
 #pragma once
 #include <iterator>
 
-
 namespace task {
 
+template <class T, class Alloc = std::allocator<T>> class list {
+  struct Node {
+    T data;
+    Node *prev;
+    Node *next;
 
-template<class T, class Alloc = std::allocator<T>>
-class list {
+    Node(const T &d = T{}, Node *p = nullptr, Node *n = nullptr)
+        : data{d}, prev{p}, next{n} {}
+    Node(T &&d, Node *p = nullptr, Node *n = nullptr)
+        : data{std::move(d)}, prev{p}, next{n} {}
+  };
+
+  using allocator_type_internal =
+  typename std::allocator_traits<Alloc>::template rebind_alloc<Node>;
+  allocator_type_internal allocator;
+
+  int count;
+  Node *first;
+  Node *last;
 
 public:
-    class iterator {
-    public:
-        using difference_type = ptrdiff_t;
-        using value_type = T;
-        using pointer = T*;
-        using reference = T&;
-        using iterator_category = std::bidirectional_iterator_tag;
+  class iterator {
+  public:
+    using difference_type = ptrdiff_t;
+    using value_type = T;
+    using pointer = T *;
+    using reference = T &;
+    using iterator_category = std::bidirectional_iterator_tag;
 
-        iterator();
-        iterator(const iterator&);
-        iterator& operator=(const iterator&);
+    iterator();
+    iterator(const iterator &);
+    iterator &operator=(const iterator &);
 
-        iterator& operator++();
-        iterator operator++(int);
-        reference operator*() const;
-        pointer operator->() const;
-        iterator& operator--();
-        iterator operator--(int);
+    iterator &operator++();
+    iterator operator++(int);
+    reference operator*() const;
+    pointer operator->() const;
+    iterator &operator--();
+    iterator operator--(int);
 
-        bool operator==(iterator other) const;
-        bool operator!=(iterator other) const;
-
-        // Your code goes here?..
-
-    private:
-        // Your code hoes here...
-    };
-
-    class const_iterator {
-        // Your code goes here...
-    };
-
-    using reverse_iterator = std::reverse_iterator<iterator>;
-    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
-
-
-    list();
-    explicit list(const Alloc& alloc);
-    list(size_t count, const T& value, const Alloc& alloc = Alloc());
-    explicit list(size_t count, const Alloc& alloc = Alloc());
-
-    ~list();
-
-    list(const list& other);
-    list(list&& other);
-    list& operator=(const list& other);
-    list& operator=(list&& other);
-
-    Alloc get_allocator() const;
-
-
-    T& front();
-    const T& front() const;
-
-    T& back();
-    const T& back() const;
-
-
-    iterator begin();
-    iterator end();
-
-    const_iterator cbegin() const;
-    const_iterator cend() const;
-
-    reverse_iterator rbegin();
-    reverse_iterator rend();
-
-    const_reverse_iterator crbegin() const;
-    const_reverse_iterator crend() const;
-
-
-    bool empty() const;
-    size_t size() const;
-    size_t max_size() const;
-    void clear();
-
-    iterator insert(const_iterator pos, const T& value);
-    iterator insert(const_iterator pos, T&& value);
-    iterator insert(const_iterator pos, size_t count, const T& value);
-
-    iterator erase(const_iterator pos);
-    iterator erase(const_iterator first, const_iterator last);
-
-
-    void push_back(const T& value);
-    void push_back(T&& value);
-    void pop_back();
-
-    void push_front(const T& value);
-    void push_front(T&& value);
-    void pop_front();
-
-    template <class... Args>
-    iterator emplace(const_iterator pos, Args&&... args);
-
-    template <class... Args>
-    void emplace_back(Args&&... args);
-
-    template <class... Args>
-    void emplace_front(Args&&... args);
-
-    void resize(size_t count);
-    void swap(list& other);
-
-
-    void merge(list& other);
-    void splice(const_iterator pos, list& other);
-    void remove(const T& value);
-    void reverse();
-    void unique();
-    void sort();
+    bool operator==(iterator other) const;
+    bool operator!=(iterator other) const;
 
     // Your code goes here?..
 
+  protected:
+    Node *current; // pointer to node in List
+    T &retrieve() const;
+
+    explicit iterator(Node *p); // protected constructor
+
+    friend class list<T>;
+
+  private:
+    // Your code hoes here...
+  };
+
+  class const_iterator {
+  public:
+    using difference_type = ptrdiff_t;
+    using value_type = T;
+    using pointer = T *;
+    using reference = T &;
+    using iterator_category = std::bidirectional_iterator_tag;
+
+    const_iterator();
+    const_iterator(const const_iterator &);
+    const_iterator &operator=(const const_iterator &);
+
+    const_iterator &operator++();
+    const_iterator operator++(int);
+    reference operator*() const;
+    pointer operator->() const;
+    const_iterator &operator--();
+    const_iterator operator--(int);
+
+    bool operator==(const_iterator other) const;
+    bool operator!=(const_iterator other) const;
+
+  protected:
+    Node *current; // pointer to node in List
+    T &retrieve() const;
+
+    explicit const_iterator(Node *p); // protected constructor
+
+    friend class list<T>;
+  };
+
+  using reverse_iterator = std::reverse_iterator<iterator>;
+  using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+  list();
+  explicit list(const Alloc &alloc);
+  list(size_t count, const T &value, const Alloc &alloc = Alloc());
+  explicit list(size_t count, const Alloc &alloc = Alloc());
+
+  ~list();
+
+  list(const list &other);
+  list(list &&other);
+  list &operator=(const list &other);
+  list &operator=(list &&other);
+
+  Alloc get_allocator() const;
+
+  T &front();
+  const T &front() const;
+
+  T &back();
+  const T &back() const;
+
+  iterator begin();
+  iterator end();
+
+  const_iterator cbegin() const;
+  const_iterator cend() const;
+
+  reverse_iterator rbegin();
+  reverse_iterator rend();
+
+  const_reverse_iterator crbegin() const;
+  const_reverse_iterator crend() const;
+
+  bool empty() const;
+  size_t size() const;
+  size_t max_size() const;
+  void clear();
+
+  iterator insert(const_iterator pos, const T &value);
+  iterator insert(const_iterator pos, T &&value);
+  iterator insert(const_iterator pos, size_t count, const T &value);
+
+  iterator erase(const_iterator pos);
+  iterator erase(const_iterator first, const_iterator last);
+
+  void push_back(const T &value);
+  void push_back(T &&value);
+  void pop_back();
+
+  void push_front(const T &value);
+  void push_front(T &&value);
+  void pop_front();
+
+  template <class... Args>
+  iterator emplace(const_iterator pos, Args &&... args);
+
+  template <class... Args> void emplace_back(Args &&... args);
+
+  template <class... Args> void emplace_front(Args &&... args);
+
+  void resize(size_t count);
+  void swap(list &other);
+
+  void merge(list &other);
+  void splice(const_iterator pos, list &other);
+  void remove(const T &value);
+  void reverse();
+  void unique();
+  void sort();
+
+  // Your code goes here?..
+
 private:
-
-    // Your code goes here...
-
+  // Your code goes here...
 };
 
 // Your template function definitions may go here...
+//#include "list.cpp"
+} // namespace task
 
-}  // namespace task
+#include "list.cpp"
